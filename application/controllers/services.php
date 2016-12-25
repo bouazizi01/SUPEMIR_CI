31c43dda847aaf57a60ba8ec92f723722947a4cd
BOUAZIZI <abderrazzakbouazizi@gmail.com>
2016-12-25 23:15:01 +0100

    premier commit de div-1

--- application/controllers/services.php
+++ application/controllers/services.php
@@ -5,7 +5,7 @@ class Services extends CI_Controller {
 public function __construct()
 {
 	parent::__construct();
-//	$this->load->helper('form');
+$this->load->helper('form');
 //	$this->load->helper('url');
 	$this->load->library('form_validation');
 	$this->load->model('service','s');
